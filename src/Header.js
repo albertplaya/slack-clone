262637dfee5b318abc9042e0547e23216f498feb
Albert Playà <albertplayavidal@gmail.com>
2020-08-30 19:06:33 +0200

    Header

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Avatar
+          className="header__avatar"
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+        />
+        <AccessTimeIcon />
+      </div>
+      <div className="header__search">
+        <SearchIcon />
+        <input placeholder="Search"></input>
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
